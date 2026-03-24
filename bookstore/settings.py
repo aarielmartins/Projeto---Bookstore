f6f9f6e194840744cc8d4c8fcf54cb705638ab9c
Ariel Martins <a.martiins.am@gmail.com>
2026-03-24 11:07:14 -0300

    Revert "Autenticacao"

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -41,8 +41,6 @@ INSTALLED_APPS = [
     "order",
     "product",
     "django_extensions",
-    'debug_toolbar',
-    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -53,7 +51,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = "bookstore.urls"
@@ -127,16 +124,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination', 'PAGE_SIZE': 5,
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication', 
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
-    ],
-}
-
-INTERNAL_IPS = [
-    '127.0.0.1',
-]
